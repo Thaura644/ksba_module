02908bc06b5163759a42a45cb646c67aa63103eb
thaura644 <jamesthaura51@gmail.com>
2023-06-12 15:30:50 +0300

    changing environment

--- kenya_school_bus_app/models/ksba_stop.py
+++ kenya_school_bus_app/models/ksba_stop.py
@@ -1,14 +1,14 @@
-from odoo import models, fields
-
-class KsbaStop(models.Model):
-    _name = 'ksba.stop'
-    _description = 'Stop'
-    # _inherit = 'ksba.route'
-
-    name = fields.Char(required=True)
-    route_id = fields.Many2one('ksba.route', string='Route', required=True)
-    sequence =  fields.Integer()
-    latitude = fields.Float()
-    longitude = fields.Float()
-    route = fields.Many2many('ksba.route', string='Routes')
-    buses = fields.Many2many('ksba.bus', string='Buses')
+# from odoo import models, fields
+#
+# class KsbaStop(models.Model):
+#     _name = 'ksba.stop'
+#     _description = 'Stop'
+#     _inherit = 'ksba.route'
+#
+#     name = fields.Char(required=True)
+#     route_id = fields.Many2one('ksba.route', string='Route', required=True)
+#     sequence =  fields.Integer()
+#     latitude = fields.Float()
+#     longitude = fields.Float()
+#     route = fields.Many2many('ksba.route', string='Routes')
+#     buses = fields.Many2many('ksba.bus', string='Buses')
